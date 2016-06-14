866447cfe1bf762f55aabb963f51b468aedd2804
773729704@qq.com <773729704@qq.com>
2016-06-14 16:03:10 +0800

    添加投票活动页面

---
+++ app/www/controllers/Vote.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/6/2
+ * Time: 19:55
+ */
+class Vote extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view("vote/index.html");
+    }
+}
